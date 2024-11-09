682859a8e0bbc41cb6770517fbc23d84da866771
DrianZetax <116927325+DrianZetax@users.noreply.github.com>
2024-11-09 14:15:00 +0700

    Update backup.js

--- backup.js
+++ backup.js
@@ -6,11 +6,10 @@ const path = require('path');
 
 const settings = require('./settings.json');
 
-
-const token = settings.token; 
-const chatidpengguna = settings.chatidpengguna;
-const dbfolder = settings.databasepath; 
-const buatpathfolderbaru = settings.buatpathfolderbaru; 
+const api = settings.api;
+const defaultChatId = settings.chatidpengguna;
+const dbfolder = settings.databasepath;
+const buatpathfolderbaru = settings.buatpathfolderbaru;
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -36,51 +35,51 @@ const buatbackup = async () => {
 };
 
 bot.onText(/!autobackup/, async (msg) => {
-    const chatidpengguna = msg.chat.id;
+    const chatId = msg.chat.id;
 
     try {
         const backupZip = await buatbackup();
-        await bot.sendMessage(chatidpengguna, 'Backup Otw');
-        await bot.sendDocument(chatidpengguna, backupZip);
+        await bot.sendMessage(chatId, 'Backup Otw');
+        await bot.sendDocument(chatId, backupZip);
     } catch (error) {
-        await bot.sendMessage(chatidpengguna, `Terjadi kesalahan saat membuat backup: ${error.message}`);
+        await bot.sendMessage(chatId, `Terjadi kesalahan saat membuat backup: ${error.message}`);
     }
 });
 
 const autobackup = new cron.CronJob('*/30 * * * * *', async () => {
     if (autoBackupActive) {
-        console.log('Melakukan auto backup...');
-        const chatId = chatidpengguna;
+        const chatId = defaultChatId;
 
         try {
             const backupZip = await buatbackup();
-            await bot.sendMessage(chatidpengguna, 'File .zip akan dikirim');
-            await bot.sendDocument(chatidpengguna, backupZip);
-            await bot.sendMessage(chatidpengguna, 'File telah siap, Terimakasih');
+            await bot.sendMessage(chatId, 'File .zip akan dikirim');
+            await bot.sendDocument(chatId, backupZip);
+            await bot.sendMessage(chatId, 'File telah siap, Terimakasih');
         } catch (error) {
             console.error(`Terjadi kesalahan: ${error.message}`);
         }
     }
 });
+
 bot.onText(/!autobackup on/, (msg) => {
-    const chatidpengguna = msg.chat.id;
+    const chatId = msg.chat.id;
     if (!autoBackupActive) {
         autoBackupActive = true;
-        bot.sendMessage(chatidpengguna, 'Auto Backup diaktifkan.');
+        bot.sendMessage(chatId, 'Auto Backup diaktifkan.');
         autobackup.start();
     } else {
-        bot.sendMessage(chatidpengguna, 'Auto Backup sudah aktif.');
+        bot.sendMessage(chatId, 'Auto Backup sudah aktif.');
     }
 });
 
 bot.onText(/!autobackup off/, (msg) => {
-    const chatidpengguna = msg.chat.id;
+    const chatId = msg.chat.id;
     if (autoBackupActive) {
         autoBackupActive = false;
-        bot.sendMessage(chatidpengguna, 'Auto Backup dinonaktifkan.');
+        bot.sendMessage(chatId, 'Auto Backup dinonaktifkan.');
         autobackup.stop();
     } else {
-        bot.sendMessage(chatidpengguna, 'Auto Backup sudah nonaktif.');
+        bot.sendMessage(chatId, 'Auto Backup sudah nonaktif.');
     }
 });
 
